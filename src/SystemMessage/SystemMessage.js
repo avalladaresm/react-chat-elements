dff8eeb46c785a6e66d2e15532d7115d067e87ed
Alejandro Valladares <avalladaresm@unitec.edu>
2020-04-04 23:41:29 -0600

    changed right mbox color, added type and color to smsgbox

--- src/SystemMessage/SystemMessage.js
+++ src/SystemMessage/SystemMessage.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import Mayre from 'mayre';
 import './SystemMessage.css';
 
 const classNames = require('classnames');
 
 export class SystemMessage extends Component {
-    render() {
-        return (
-            <div className={classNames("rce-container-smsg", this.props.className)}>
-                <div
-                    className='rce-smsg'>
-                    <div className="rce-smsg-text">
-                        {this.props.text}
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Mayre
+					of={
+						<div className={classNames('rce-container-smsg', this.props.className)}>
+							<div className="rce-smsg">
+								<div className="rce-smsg-text">{this.props.text}</div>
+							</div>
+						</div>
+					}
+					when={this.props.type === 'text'}
+				/>
+				<Mayre
+					of={
+						<div className={classNames('rce-container-smsgd', this.props.className)}>
+							<div className="rce-smsg">
+								<div className="rce-smsg-text">{this.props.date}</div>
+							</div>
+						</div>
+					}
+					when={this.props.type === 'date'}
+				/>
+			</div>
+		);
+	}
 }
 
 export default SystemMessage;
